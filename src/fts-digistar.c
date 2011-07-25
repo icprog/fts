354108637776799d0cb3ba3a6392be59c44f6499
Igor Kramer Pinotti <ipinotti@pd3.com.br>
2011-07-25 11:13:44 -0300

    adding system

--- src/fts-digistar.c
+++ src/fts-digistar.c
@@ -10,8 +10,137 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/ioctl.h>
+#include <signal.h>
+#include <syslog.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <time.h>
 
-int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+#include <net/if.h>
+#include <netinet/ip_icmp.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#include <librouter/options.h>
+
+#include "fts-digistar.h"
+
+#ifdef OPTION_MODEM3G
+#include <librouter/dev.h> /* get_dev_link */
+#include <librouter/usb.h>
+#include <librouter/device.h>
+#include <librouter/modem3G.h>
+#include <librouter/quagga.h>
+#include <librouter/str.h>
+#endif
+
+/* Temporary*/
+#define OPTION_FTS_DIGISTAR
+
+
+#if defined (OPTION_FTS_DIGISTAR)
+
+static void fts_digistar_enable_log(int enable)
+{
+	freopen("/var/log/fts-digistar_log", "a+", stdin);
+	freopen("/var/log/fts-digistar_log", "a+", stdout);
+	freopen("/var/log/fts-digistar_log", "a+", stderr);
+}
+
+static void fts_digistar_welcome_string_show()
+{
+	printf("\n\tWelcome to Digistar %s\n", SERIES);
+	printf("\tPress Y within 3 seconds to enter in test...\n\n");
+}
+
+static int fts_digistar_test_startup()
+{
+	char cmd=0;
+	int ret=-1;
+	pid_t pID, pID2,p;
+
+	fts_digistar_welcome_string_show();
+
+	switch (pID = fork()) {
+		case -1:
+			syslog(LOG_ERR, "Could not startup FTS-Digistar\n");
+			break;
+
+		case 0: /* Child */
+			sleep(3);
+			exit(EXIT_SUCCESS);
+			break;
+
+		default: /* Parent */
+			pID2 = fork();
+			if (pID2 == 0) {// child
+				scanf("%c", &cmd);
+				if (cmd == 'Y' || cmd == 'y')
+					ret = 1;
+				else
+					ret = 0;
+				kill( pID, SIGKILL );
+				break;
+			} else if (pID2 < 0) {          // failed to fork
+				syslog(LOG_ERR, "Could not startup FTS-Digistar\n");
+				break;
+			} else {
+				waitpid(pID,NULL,0);// parent
+				kill(pID2,SIGKILL);
+				ret = 0;
+				break;
+			}
+	}
+	waitpid(pID2,NULL,0);
+	return ret;
+}
+
+static int main_fts()
+{
+	int i = 0,test = 0;
+	test = fts_digistar_test_startup();
+	printf("|TEST____ %d\n",test);
+	if (!test){
+		printf("DEU MERDA - pid %d\n",getpid());
+		return 0;
+	}
+
+	printf("TESTANDO - pid %d\n",getpid());
+	int b=5;
+	while (b){
+		b--;
+		printf("b\n");
+		sleep(1);
+	}
+
+	return 0;
+}
+
+int main(int argc, char **argv) {
+	syslog(LOG_INFO, "FTS-Digistar WORKING ...\n");
+
+	main_fts();
+
+	printf("fechou");
+
+	syslog(LOG_INFO, "FTS-Digistar exiting ...\n");
 	return EXIT_SUCCESS;
 }
+
+#else /* OPTION_FTS_DIGISTAR */
+int main(int argc, char **argv)
+{
+	syslog(LOG_INFO, "FTS-Digistar not supported, exiting ...\n");
+	return 0;
+}
+#endif /* OPTION_FTS_DIGISTAR */
--- src/fts-digistar.h
+++ src/fts-digistar.h
@@ -8,5 +8,42 @@
 #ifndef FTS_DIGISTAR_H_
 #define FTS_DIGISTAR_H_
 
+#include <librouter/options.h>
+
+#ifdef CONFIG_DIGISTAR_3G
+#define SERIES "RCG-SERIES ROUTER"
+
+#ifdef CONFIG_DIGISTAR_RCG1000
+#define MODEL "RCG-1000"
+#endif
+
+#ifdef CONFIG_DIGISTAR_RCG800
+#define MODEL "RCG-800"
+#endif
+
+#ifdef CONFIG_DIGISTAR_RCG700
+#define MODEL "RCG-700"
+#endif
+
+#elif CONFIG_DIGISTAR_EFM
+#define SERIES "ETL-SERIES ROUTER/BRIDGE"
+
+#ifdef CONFIG_DIGISTAR_ETL8110
+#define MODEL "RCG-1000"
+#endif
+
+#ifdef CONFIG_DIGISTAR_ETL8210
+#define MODEL "RCG-800"
+#endif
+
+#ifdef CONFIG_DIGISTAR_ETL8221
+#define MODEL "RCG-700"
+#endif
+
+#ifdef CONFIG_DIGISTAR_ETL8421
+#define MODEL "ETL-8421"
+#endif
+
+#endif
 
 #endif /* FTS_DIGISTAR_H_ */
