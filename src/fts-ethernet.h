575c0673600dccc737df8639928a56ebeacc06b8
Thomás Alimena Del Grande <tgrande@pd3.com.br>
2011-07-28 19:05:24 -0300

    Fix WAN IP address definitions

--- src/fts-ethernet.h
+++ src/fts-ethernet.h
@@ -10,24 +10,23 @@
 
 /* General Defines*/
 /* WAN */
-#define WAN_IP "10.1.1.99"
-#define WAN_MASK "255.255.255.0"
-#define WAN_DEV "eth1"
+#define WAN_IP 		"10.0.0.90"
+#define WAN_MASK 	"255.255.255.0"
+#define WAN_DEV 	"eth1"
 
 /* WAN HOST PING */
-#define HOST_0_PING "10.1.1.5"
-
+#define HOST_0_PING 	"10.0.0.1"
 
 /* LAN */
-#define LAN_IP "10.1.1.90"
-#define LAN_MASK "255.255.255.0"
-#define LAN_DEV "eth0"
+#define LAN_IP 		"10.1.1.90"
+#define LAN_MASK 	"255.255.255.0"
+#define LAN_DEV 	"eth0"
 
 /* LAN HOST PING */
-#define HOST_1_PING "10.1.1.1"
-#define HOST_2_PING "10.1.1.2"
-#define HOST_3_PING "10.1.1.3"
-#define HOST_4_PING "10.1.1.4"
+#define HOST_1_PING 	"10.1.1.1"
+#define HOST_2_PING 	"10.1.1.2"
+#define HOST_3_PING 	"10.1.1.3"
+#define HOST_4_PING 	"10.1.1.4"
 
 struct fts_test ethwan_test;
 struct fts_test ethlan_test;
